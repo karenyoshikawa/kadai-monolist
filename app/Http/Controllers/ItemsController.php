4fc520b7886e469b6dffa00a68abd781b6ef1b1f
karen.yoshikawa <karen.yoshikawa@rakuten.com>
2018-06-27 01:13:11 +0000

    want

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -1,3 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Controllers\Controller;
+
 use \App\Item;
 
   class ItemsController extends Controller
@@ -32,5 +40,15 @@ use \App\Item;
             'keyword' => $keyword,
             'items' => $items,
         ]);
+    }
+     public function show($id)
+    {
+      $item = Item::find($id);
+      $want_users = $item->want_users;
+
+      return view('items.show', [
+          'item' => $item,
+          'want_users' => $want_users,
+      ]);
     }
   }
